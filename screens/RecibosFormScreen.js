32592257480b76fea0d00ba736d21c22d8d9b74b
Paul charaja <axebot654@gmail.com>
2025-07-31 16:20:56 -0500

    datos

--- screens/RecibosFormScreen.js
+++ screens/RecibosFormScreen.js
@@ -4,6 +4,12 @@ import { View, Text, TextInput, TouchableOpacity, StyleSheet, Alert, ScrollView 
 import { supabase } from '../lib/supabase';
 import { Picker } from '@react-native-picker/picker';
 import {  Button } from 'react-native-paper';
+import { Dropdown } from 'react-native-element-dropdown';
+
+const data = [
+  { label: 'Ingreso', value: 'Ingreso' },
+  { label: 'Egreso', value: 'Egreso' },
+];
 
 export default function RecibosFormScreen({ navigation }) {
   const [fecha, setFecha] = useState(new Date());
@@ -12,7 +18,26 @@ export default function RecibosFormScreen({ navigation }) {
 	const [descripcion, setDescripcion] = useState('');
   const [nombre, setNombre] = useState('');
   const [monto, setMonto] = useState('');
-   const [tipoRecibo, setTipoRecibo] = useState('Ingreso'); // Nuevo selector
+  const [tipoRecibo, setTipoRecibo] = useState('Ingreso'); // Nuevo selector
+
+  const [primicia,setPrimicia] = useState('');
+  const [diezmo, setDiezmo] = useState('');
+  const [oobres, setPobres] = useState('');
+  const [agradecimiento, setAgradecimiento] = useState('');
+  const [esc_sabatica, setEscsabatica] = useState('');
+  const [jovenes, setJovenes] = useState('');
+  const [adolescentes, setAdolescentes] = useState('');
+  const [ninos, setNinos] = useState('');
+  const [educacion, setEducacion] = useState('');
+  const [salud, setSalud] = useState('');
+  const [obra_mis, setObramis] = useState('');
+  const [musica, setMusica] = useState('');
+  const [renuevatv, setRenuevatv] = useState('');
+  const [primer_sabado, setPrimersab] = useState('');
+  const [sem_oracion, setSemorac] = useState('');
+  const [mis_extranj, setMisextran] = useState('');
+  const [construccion, setConstruc] = useState('');
+  const [diversos, setDiversos] = useState('');
 
 	const formatDate = (date) => date.toLocaleDateString('en-CA');
 
@@ -47,16 +72,20 @@ export default function RecibosFormScreen({ navigation }) {
 	
   return (
     <ScrollView contentContainerStyle={styles.container}>
-
-      <Text style={styles.label}>Tipo de Registro</Text>
-      <View style={styles.pickerContainer}>
-        <Picker
-          selectedValue={tipoRecibo}
-          onValueChange={(itemValue) => setTipoRecibo(itemValue)}
-        >
-          <Picker.Item label="Ingreso" value="Ingreso" />
-          <Picker.Item label="Egreso" value="Egreso" />
-        </Picker>
+      <View style={styles.dropdownContainer}>
+        <Dropdown
+          style={styles.dropdown}
+          placeholderStyle={styles.placeholderStyle}
+          selectedTextStyle={styles.selectedTextStyle}
+          data={data}
+          labelField="label"
+          valueField="value"
+          placeholder="Selecciona tipo"
+          value={tipoRecibo}
+          onChange={(item) => {
+            setTipoRecibo(item.value);
+          }}
+        />
       </View>
 
 			<Text style={styles.label}>Nuevo Recibo</Text>
@@ -137,5 +166,23 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     fontWeight: 'bold',
     fontSize: 15,
+  },
+  dropdownContainer: {
+    marginBottom: 20,
+  },
+  dropdown: {
+    height: 50,
+    borderColor: '#ccc',
+    borderWidth: 1,
+    borderRadius: 8,
+    paddingHorizontal: 12,
+  },
+  placeholderStyle: {
+    color: '#999',
+    fontSize: 16,
+  },
+  selectedTextStyle: {
+    fontSize: 16,
+    color: '#000',
   },
 });
